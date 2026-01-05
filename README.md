3c8dafd0502915345c05bc3140d05f16a9e8ecfb
Brainsmatics-Opticalimaging <luosihao.simon@gmail.com>
2026-01-05 19:40:14 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
 # AB-MRUNet
-Coming soon
+
+We recommend using a conda environment over a virtual environment. This repository is base on the official PyTorch code for the FR-UNet( ['Full-Resolution Network 
+and Dual-Threshold Iteration for Retinal Vessel and Coronary Angiograph Segmentation'](https://ieeexplore.ieee.org/abstract/document/9815506).)
+
+## Setup repository and environment
+
+```
+# clone our repository
+git clone git@github.com:Brainsmatics-Opticalimaging/AB-MRUNet.git
+cd AB-MRUNet
+
+# Create a Conda Environment
+conda create --name AB-MRUNet
+
+# Activate the Conda Environment
+conda activate AB-MRUNet
+
+# Install Required Packages
+pip install -r requirements.txt
+```
+
+## Datasets processing
+
+Choose a path to create a folder with the dataset name and download datasets [DRIVE](https://www.dropbox.com/sh/z4hbbzqai0ilqht/AAARqnQhjq3wQcSVFNR__6xNa?dl=0),[CHASEDB1](https://blogs.kingston.ac.uk/retinal/chasedb1/),[STARE](https://cecas.clemson.edu/~ahoover/stare/probing/index.html),[CHUAC](https://figshare.com/s/4d24cf3d14bc901a94bf), and [DCA1](http://personal.cimat.mx:8181/~ivan.cruz/DB_Angiograms.html). Type this in terminal to run the data_process.py file
+
+```
+python data_process.py -dp DATASET_PATH -dn DATASET_NAME
+```
+
+## Training
+
+Type this in terminal to run the train.py file
+
+```
+python train.py -dp DATASET_PATH
+```
+
+## Test
+
+Type this in terminal to run the test.py file
+
+```
+python test.py -dp DATASET_PATH -wp WEIGHT_FILE_PATH
+```
+
+We have prepared the pre-trained models for both datasets in the folder 'pretrained_weights'. To replicate the results in the paper, directly run the following commands
+
+```
+python test.py -dp DATASET_PATH -wp pretrained_weights/DATASET_NAME
+```
+
+## Inference
+
+Type this in terminal to run the prediction.py file
+
+```
+python prediction.py
+```
+
+## Pretrained Model/weights and Dataset download（Coming soon）:
+
+Please download the model folder from the following Baiduyun Drive link: （coming soon）
+
+
+
---
+++ prediction.py
@@ -0,0 +1,89 @@
+import numpy as np
+import torch
+import cv2
+import models
+from torchvision import transforms
+from PIL import Image
+import os
+import argparse
+import logging
+import os
+from utils.helpers import get_instance, seed_torch
+from torchvision.transforms import Grayscale, Normalize, ToTensor
+from ruamel.yaml import safe_load, YAML
+import torch.nn as nn
+def normalization(imgs):
+
+    mean = torch.mean(imgs)
+    std = torch.std(imgs)
+    n = Normalize([mean], [std])(imgs)
+    n = (n - torch.min(n)) / (torch.max(n) - torch.min(n))
+
+    return n
+
+# 预处理
+transform = transforms.ToTensor()
+
+yaml = YAML(typ='safe', pure=True)
+with open('config.yaml', encoding='utf-8') as file:
+       # CFG = Bunch(safe_load(file))
+    CFG = yaml.load(file)
+# 加载模型
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+net = get_instance(models, 'model', CFG)
+net = nn.DataParallel(net.to('cpu'))
+net.to(device)
+
+
+
+#net.load_state_dict(torch.load('checkpoints/checkpoint_epoch30.pth', map_location=device))
+state_dict = torch.load('pretrained_weights/checkpoint-epoch33.pth', map_location=torch.device('cpu'))
+
+net.load_state_dict(state_dict['state_dict'])
+
+
+# 测试模式
+net.eval()
+# 读取所有图片路径
+tests_path = os.listdir('predict/1/')  # 获取 './predict/' 路径下所有文件,这里的路径只是里面文件的路径
+
+with torch.no_grad():  # 预测的时候不需要计算梯度
+    for test_path in tests_path:  # 遍历每个predict的文件
+        save_pre_path = 'predict/result_1/' + test_path.split('.')[-2] + '_res.tif'
+        save_pre_path1 = 'predict/result_1/' + test_path.split('.')[-2] + '_r.tif' # 将保存的路径按照原图像的后缀，按照数字排序保存
+        img_all = Image.open('predict/1/' + test_path)  # 预测图片的路径
+        width, height = img_all.size[0], img_all.size[1]  # 保存图像的大小
+
+        base=1024
+        a=3
+        b=3
+        predict_all=np.zeros((height,width))
+
+        for i in range(a):
+            for j in range(b):
+
+                select=(i*base,j*base,(i+1)*base,(j+1)*base)
+                img=img_all.crop(select)
+
+                img = transform(img)
+
+                img=normalization(img)
+
+                img = torch.unsqueeze(img, dim=0)  # 扩展图像的维度
+
+                pred = net(img)  # 网络预测
+
+                pred = torch.squeeze(pred)  # 将(batch、channel)维度去掉print(pred)
+                #print(pred)
+               # pred = pred.argmax(dim=0)
+
+                predict = torch.sigmoid(pred).cpu().detach().numpy()
+                predict_all[j*base:(j+1)*base,i*base:(i+1)*base]=predict
+
+
+        predict_b = np.where(predict_all >= 0.65, 1, 0)
+
+        cv2.imwrite(save_pre_path1, np.uint8(predict_all * 255))
+        cv2.imwrite(save_pre_path, np.uint8(predict_b*255))
+        print(test_path)
+
